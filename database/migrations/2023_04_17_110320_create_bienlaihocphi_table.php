8a3f957b3c253d7332b8f78ae68e7647c2ec23dd
Phat <ntpbot1@gmail.com>
2023-04-17 18:21:56 +0700

    xem lan 1

--- database/migrations/2023_04_17_110320_create_bienlaihocphi_table.php
+++ database/migrations/2023_04_17_110320_create_bienlaihocphi_table.php
@@ -14,16 +14,17 @@ class CreateBienlaihocphiTable extends Migration
     public function up()
     {
         Schema::table('bienlaihocphi', function (Blueprint $table) {
-            $table->string('MaBL',10);
+            $table->string('MaBL', 10);
             $table->dateTime('NgayDong');
-            $table->string('SoTien',11);
-            $table->string('GhiChu',100);
-            $table->string('MaLop',10);
-            $table->string('MaHS',10);
+            $table->string('SoTien', 11);
+            $table->string('GhiChu', 100);
+            $table->string('MaLop', 10);
+            $table->string('MaHS', 10);
             $table->primary('MaBL');
             $table->foreign('MaHS')->references('MaHS')->on('hocsinh');
             $table->foreign('MaLop')->references('MaLop')->on('lopnangkhieu');
             $table->engine = 'InnoDB';
+            $table->timestamps();
         });
     }
 
@@ -34,8 +35,6 @@ class CreateBienlaihocphiTable extends Migration
      */
     public function down()
     {
-        Schema::table('bienlaihocphi', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('bienlaihocphi');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,130 +29,146 @@ use App\Http\Controllers\Page\SearchController;
 */
 //=====================================Page============================================
 //Main
-Route::get('/',[MainController::class,'index']);
+Route::get('/', [MainController::class, 'index']);
 //NSX
-Route::get('/nsx/{nsx}',[MainController::class,'show']);
+Route::get('/nsx/{nsx}', [MainController::class, 'show']);
 //Product_detail
-Route::get('/detail/{sp}',[MainController::class,'detail']);
+Route::get('/detail/{sp}', [MainController::class, 'detail']);
 //Cart
 // Route::get('/cart',[CartController::class,'show']);
 // Route::post('/cart',[CartController::class,'add']);
 // Route::post('/cart',[CartController::class,'add']);
-Route::prefix('cart')->group(function(){
-    Route::get('/',[CartController::class,'show']);
-    Route::post('/',[CartController::class,'add']);
-    Route::get('/delete/{id}',[CartController::class,'delete']);
-    Route::post('/update',[CartController::class,'update']);
+Route::prefix('cart')->group(function () {
+    Route::get('/', [CartController::class, 'show']);
+    Route::post('/', [CartController::class, 'add']);
+    Route::get('/delete/{id}', [CartController::class, 'delete']);
+    Route::post('/update', [CartController::class, 'update']);
 });
 //Login
-Route::get('/login',[PageLoginController::class,'login']);
-Route::post('/login',[PageLoginController::class,'show']);
+Route::get('/login', [PageLoginController::class, 'login']);
+Route::post('/login', [PageLoginController::class, 'show']);
 //Register
-Route::get('/register',[PageLoginController::class,'register']);
-Route::post('/register',[PageLoginController::class,'add']);
+Route::get('/register', [PageLoginController::class, 'register']);
+Route::post('/register', [PageLoginController::class, 'add']);
 //Logout
-Route::get('/logout',[PageLoginController::class,'logout']);
+Route::get('/logout', [PageLoginController::class, 'logout']);
 //Checkout
-Route::get('/checkout',[CheckoutController::class,'checkout']);
-Route::get('/checkout/show',[CheckoutController::class,'show']);
-Route::get('/checkout/detail/{id_dh}',[CheckoutController::class,'detail']);
+Route::get('/checkout', [CheckoutController::class, 'checkout']);
+Route::get('/checkout/show', [CheckoutController::class, 'show']);
+Route::get('/checkout/detail/{id_dh}', [CheckoutController::class, 'detail']);
 //Search
-Route::post('/search',[SearchController::class,'search']);
+Route::post('/search', [SearchController::class, 'search']);
 
 
 
 
 //=====================================Admin_login============================================
-Route::get('/admin/users/login',[LoginController::class,'index'])->name('login');
-Route::post('/admin/users/login/store',[LoginController::class,'store']);
-Route::get('/admin/users/logout',[LoginController::class,'logout']);
+Route::get('/admin/users/login', [LoginController::class, 'index'])->name('login');
+Route::post('/admin/users/login/store', [LoginController::class, 'store']);
+Route::get('/admin/users/logout', [LoginController::class, 'logout']);
 
 //=====================================Admin============================================
-    Route::prefix('admin')->group(function(){
-        Route::get('/',[LoginController::class,'index'])->name('admin');
-        Route::get('/main',[LoginController::class,'show']);
+Route::prefix('admin')->group(function () {
+    Route::get('/', [LoginController::class, 'index'])->name('admin');
+    Route::get('/main', [LoginController::class, 'show']);
     //Users
-    Route::middleware('adminLogin')->prefix('users')->group(function(){
-        Route::get('add',[MemberController::class,'index']);
-        Route::post('add',[MemberController::class,'add']);
+    Route::middleware('adminLogin')->prefix('users')->group(function () {
+        Route::get('add', [MemberController::class, 'index']);
+        Route::post('add', [MemberController::class, 'add']);
     });
     //Menu
-    Route::middleware('adminLogin')->prefix('menus')->group(function(){
-        Route::get('add',[MenuController::class,'create']);
-        Route::post('add',[MenuController::class,'store']);
-        Route::get('list',[MenuController::class,'index']);
-        Route::get('edit/{nsx}',[MenuController::class,'show']);
-        Route::post('edit/{nsx}',[MenuController::class,'update']);
-        Route::DELETE('destroy',[MenuController::class,'destroy']);
+    Route::middleware('adminLogin')->prefix('menus')->group(function () {
+        Route::get('add', [MenuController::class, 'create']);
+        Route::post('add', [MenuController::class, 'store']);
+        Route::get('list', [MenuController::class, 'index']);
+        Route::get('edit/{nsx}', [MenuController::class, 'show']);
+        Route::post('edit/{nsx}', [MenuController::class, 'update']);
+        Route::DELETE('destroy', [MenuController::class, 'destroy']);
     });
     //Product
-    Route::middleware('adminLogin')->prefix('product')->group(function(){
-        Route::get('add',[ProductController::class,'create']);
-        Route::post('add',[ProductController::class,'store']);
-        Route::get('list',[ProductController::class,'index']);
-        Route::get('edit/{sp}',[ProductController::class,'edit']);
-        Route::post('edit/{sp}',[ProductController::class,'update']);
-        Route::get('destroy/{sp}',[ProductController::class,'destroy']);
+    Route::middleware('adminLogin')->prefix('product')->group(function () {
+        Route::get('add', [ProductController::class, 'create']);
+        Route::post('add', [ProductController::class, 'store']);
+        Route::get('list', [ProductController::class, 'index']);
+        Route::get('edit/{sp}', [ProductController::class, 'edit']);
+        Route::post('edit/{sp}', [ProductController::class, 'update']);
+        Route::get('destroy/{sp}', [ProductController::class, 'destroy']);
     });
     //Upload
-    Route::get('upload/services',[UploadController::class,'store']);
+    Route::get('upload/services', [UploadController::class, 'store']);
     //Customer
-    Route::middleware('adminLogin')->prefix('customer')->group(function(){
-        Route::get('list',[MemberController::class,'show']);
-        Route::get('list2',[MemberController::class,'show2']);
-        Route::get('edit/{id}',[MemberController::class,'edit']);
-        Route::post('edit/{id}',[MemberController::class,'update']);
-        Route::post('update/{id}',[MemberController::class,'update2']);
-        Route::get('delete/{id}',[MemberController::class,'delete']);
-
+    Route::middleware('adminLogin')->prefix('customer')->group(function () {
+        Route::get('list', [MemberController::class, 'show']);
+        Route::get('list2', [MemberController::class, 'show2']);
+        Route::get('edit/{id}', [MemberController::class, 'edit']);
+        Route::post('edit/{id}', [MemberController::class, 'update']);
+        Route::post('update/{id}', [MemberController::class, 'update2']);
+        Route::get('delete/{id}', [MemberController::class, 'delete']);
     });
     //Class
-    Route::middleware('adminLogin')->prefix('class')->group(function(){
-        Route::get('list',[ClassController::class,'show']);
-        Route::get('add',[ClassController::class,'show2']);
-        Route::post('add',[ClassController::class,'add']);
-        Route::get('edit/{id}',[ClassController::class,'edit']);
-        Route::post('edit/{id}',[ClassController::class,'update']);
-        Route::get('delete/{id}',[ClassController::class,'delete']);
+    Route::middleware('adminLogin')->prefix('class')->group(function () {
+        Route::get('list', [ClassController::class, 'show']);
+        Route::get('add', [ClassController::class, 'show2']);
+        Route::post('add', [ClassController::class, 'add']);
+        Route::get('edit/{id}', [ClassController::class, 'edit']);
+        Route::post('edit/{id}', [ClassController::class, 'update']);
+        Route::get('delete/{id}', [ClassController::class, 'delete']);
     });
     //Parent
-    Route::middleware('adminLogin')->prefix('parent')->group(function(){
-        Route::get('list',[ParentController::class,'show']);
-        Route::get('add',[ParentController::class,'show2']);
-        Route::post('add',[ParentController::class,'add']);
-        Route::get('edit/{id}',[ParentController::class,'edit']);
-        Route::post('edit/{id}',[ParentController::class,'update']);
-        Route::get('delete/{id}',[ParentController::class,'delete']);
+    Route::middleware('adminLogin')->prefix('parent')->group(function () {
+        Route::get('list', [ParentController::class, 'show']);
+        Route::get('add', [ParentController::class, 'show2']);
+        Route::post('add', [ParentController::class, 'add']);
+        Route::get('edit/{id}', [ParentController::class, 'edit']);
+        Route::post('edit/{id}', [ParentController::class, 'update']);
+        Route::get('delete/{id}', [ParentController::class, 'delete']);
     });
     //Brunch
-    Route::middleware('adminLogin')->prefix('brunch')->group(function(){
-        Route::get('list',[BrunchController::class,'show']);
-        Route::get('add',[BrunchController::class,'show2']);
-        Route::post('add',[BrunchController::class,'add']);
-        Route::get('edit/{id}',[BrunchController::class,'edit']);
-        Route::post('edit/{id}',[BrunchController::class,'update']);
-        Route::get('delete/{id}',[BrunchController::class,'delete']);
+    Route::middleware('adminLogin')->prefix('brunch')->group(function () {
+        Route::get('list', [BrunchController::class, 'show']);
+        Route::get('add', [BrunchController::class, 'show2']);
+        Route::post('add', [BrunchController::class, 'add']);
+        Route::get('edit/{id}', [BrunchController::class, 'edit']);
+        Route::post('edit/{id}', [BrunchController::class, 'update']);
+        Route::get('delete/{id}', [BrunchController::class, 'delete']);
     });
     //Food
-    Route::middleware('adminLogin')->prefix('food')->group(function(){
-        Route::get('list',[FoodController::class,'show']);
-        Route::get('add',[FoodController::class,'show2']);
-        Route::post('add',[FoodController::class,'add']);
-        Route::get('edit/{id}',[FoodController::class,'edit']);
-        Route::post('edit/{id}',[FoodController::class,'update']);
-        Route::get('delete/{id}',[FoodController::class,'delete']);
+    Route::middleware('adminLogin')->prefix('food')->group(function () {
+        Route::get('list', [FoodController::class, 'show']);
+        Route::get('add', [FoodController::class, 'show2']);
+        Route::post('add', [FoodController::class, 'add']);
+        Route::get('edit/{id}', [FoodController::class, 'edit']);
+        Route::post('edit/{id}', [FoodController::class, 'update']);
+        Route::get('delete/{id}', [FoodController::class, 'delete']);
     });
+    //Food
+    Route::middleware('adminLogin')->prefix('fees')->group(function () {
+        Route::get('list', [FoodController::class, 'show']);
+        Route::get('add', [FoodController::class, 'show2']);
+        Route::post('add', [FoodController::class, 'add']);
+        Route::get('edit/{id}', [FoodController::class, 'edit']);
+        Route::post('edit/{id}', [FoodController::class, 'update']);
+        Route::get('delete/{id}', [FoodController::class, 'delete']);
     });
-    
-    //Customer
-    // Route::prefix('customer')->group(function(){
-    //     Route::get('list',[MemberController::class,'show']);
-    // });
-    //Orders
-    Route::middleware('adminLogin')->prefix('order')->group(function(){
-        Route::get('list',[OrderController::class,'show']);
-        Route::post('update/{id}',[OrderController::class,'update']);
+    //Food
+    Route::middleware('adminLogin')->prefix('food')->group(function () {
+        Route::get('list', [FoodController::class, 'show']);
+        Route::get('add', [FoodController::class, 'show2']);
+        Route::post('add', [FoodController::class, 'add']);
+        Route::get('edit/{id}', [FoodController::class, 'edit']);
+        Route::post('edit/{id}', [FoodController::class, 'update']);
+        Route::get('delete/{id}', [FoodController::class, 'delete']);
     });
+});
+
+//Customer
+// Route::prefix('customer')->group(function(){
+//     Route::get('list',[MemberController::class,'show']);
+// });
+//Orders
+Route::middleware('adminLogin')->prefix('order')->group(function () {
+    Route::get('list', [OrderController::class, 'show']);
+    Route::post('update/{id}', [OrderController::class, 'update']);
+});
     //Main
 // });
-
